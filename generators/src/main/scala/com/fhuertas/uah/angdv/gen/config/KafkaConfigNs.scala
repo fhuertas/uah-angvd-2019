1a6f8b3b4fe0fe0105c90379551798a201898235
fhuertas <francisco@fhuertas.com>
2019-01-27 20:49:35 +0100

    GeneratorsV1

---
+++ generators/src/main/scala/com/fhuertas/uah/angdv/gen/config/KafkaConfigNs.scala
@@ -0,0 +1,12 @@
+package com.fhuertas.uah.angdv.gen.config
+
+object KafkaConfigNs {
+  val Consumer  = "kafka.consumer"
+  val TotalTime = "time.total"
+  val BatchTime = "time.batch"
+
+  val MediaWords       = "words.media"
+  val DeviationWords   = "words.deviation"
+  val ElementsPerBatch = "elements.per.batch"
+  val TopicName        = "topic"
+}
