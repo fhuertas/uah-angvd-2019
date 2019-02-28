7a8e7b13d27883f578c68d91e06e976deb56e14f
fhuertas <francisco@fhuertas.com>
2019-02-28 20:41:29 +0100

    Fixed exercise 6

--- ejercicios-py/ejercicios/ejercicio5/solution.py
+++ ejercicios-py/ejercicios/ejercicio5/solution.py
@@ -35,9 +35,9 @@ def job1():
     words = lines.withColumn("value", remove_chars("value"))
 
     # Generate running word count
-    word_counts = words.withColumn("value", sum("value"))
+    # word_counts = words.withColumn("value", sum("value"))
 
-    query = word_counts \
+    query = words \
         .writeStream \
         .outputMode("complete") \
         .format("console") \
@@ -72,6 +72,61 @@ def job2():
         .format("console") \
         .start()
 
+    query.awaitTermination()
+
+
+def job3():
+    # Creando el contexto. Aqui se pueden establecer configuración del Job de Spark
+    # o delegarlas a la llamada de spark-submit
+    spark = SparkSession \
+        .builder \
+        .appName("StructuredNetworkWordCount") \
+        .getOrCreate()
+
+    # Creando el source de datos. y lo transforma en un dataset en streaming
+    # * readStream: indicando que es Streaming
+    # * format: Indica que connector se va a usar, entre los posibles valores está
+    #           kafka, socket, console. etc...
+    # * option/options: Opciones del conector
+    # * load. Finaliza la construcción
+
+    lines = spark \
+        .readStream \
+        .format("socket") \
+        .option("host", "localhost") \
+        .option("port", 9999) \
+        .load()
+
+    # Operaciones de transformacion y manipulación. este caso divide el texto por " "
+    # y lo establece como una tabla con la columna word
+    words = lines.select(
+        explode(
+            split(lines.value, " ")
+        ).alias("word")
+    )
+
+    # Operacion de agregacion por id. une todos los campos en una lista y posteriormente la cuenta
+    word_counts = words.groupBy("word").count()
+
+    # Establece el destino del dataset.
+    # * writeStream: establece destino en streaming
+    # * outputMode: Indica como se van actualizar los campos,
+    #     * complete : Copia todos los datos del dataset cada vez
+    #     * append: Copia solo los campos nuevos del dataset
+    #     * update: Copia solo los datos que se han actualizado en el dataset
+    # * format: donde van a copiarse los datos, al igual que el input indica el connector como
+    #             kafka.
+    # * option/options: Indica opciones especificas del conector
+    # * start: operacion que arranca el proceso de streaming.
+
+    query = word_counts \
+        .writeStream \
+        .outputMode("complete") \
+        .format("console") \
+        .start()
+
+    # Al ser la operacion asycrona, esta llamada espera a que acabe la query
+    # (que puede no ser hasta que falle)
     query.awaitTermination()
 
 
