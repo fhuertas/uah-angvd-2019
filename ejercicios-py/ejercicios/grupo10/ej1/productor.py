ee4a06bddb502e60f3418857073ac4dc9c4ddfc5
fhuertas <francisco@fhuertas.com>
2019-04-29 20:43:02 +0200

    Productor original

---
+++ ejercicios-py/ejercicios/grupo10/ej1/productor.py
@@ -0,0 +1,28 @@
+from confluent_kafka import Producer
+import random
+
+def productor():
+    p = Producer({'bootstrap.servers': 'localhost:9092'})
+
+    def delivery_report(err, msg):
+        """ Called once for each message produced to indicate delivery result.Triggered by poll() or flush(). """
+        if err is not None:
+            print('Message delivery failed: {}'.format(err))
+        else:
+            print('Messagedelivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+    while True:
+        # Trigger any available delivery report callbacks from previous produce() calls
+        p.poll(0)
+        nums = str(random.randint(-10, 40)) + ',' + "{:.6f}".format(random.uniform(-40.0, 40.0)) \
+               + ',' + "{:.6f}".format(random.uniform(-40.0, 40.0))
+        # Asynchronously produce a message, the delivery report callback
+        # will be triggered from poll() above, or flush() below, when the message has
+        # been successfully delivered or failed permanently.
+        p.produce('topic', nums.encode('utf-8'), callback=delivery_report)
+        # Wait for any outstanding messages to be delivered and delivery report
+        # callbacks to be triggered.
+        p.flush()
+
+
+productor()
