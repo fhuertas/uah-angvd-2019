fa42ac3d59b2574ed4990aa0589be97ac5104f14
fhuertas <francisco@fhuertas.com>
2019-04-29 20:51:00 +0200

    Correcciones

--- ejercicios-py/ejercicios/grupo10/ej1/productor.py
+++ ejercicios-py/ejercicios/grupo10/ej1/productor.py
@@ -1,6 +1,6 @@
 from confluent_kafka import Producer
 import random
-
+import time
 def productor():
     p = Producer({'bootstrap.servers': 'localhost:9092'})
 
@@ -19,6 +19,7 @@ def productor():
         # Asynchronously produce a message, the delivery report callback
         # will be triggered from poll() above, or flush() below, when the message has
         # been successfully delivered or failed permanently.
+        time.sleep(1)
         p.produce('topic', nums.encode('utf-8'), callback=delivery_report)
         # Wait for any outstanding messages to be delivered and delivery report
         # callbacks to be triggered.
